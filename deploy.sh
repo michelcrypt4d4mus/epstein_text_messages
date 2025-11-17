f69bad43b0e8bec6a80c24d7f3e92f0037f3e98b
ashariyar <ashariyar@users.noreply.github.com>
2025-11-17 14:50:33 -0500

    bannon

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if any_uncommitted_changes; then
+    echo "Uncommitted changes; halting."
+    exit
+fi
+
+./extract_text.sh
+git commit -am"Update HTML"
+git checkout gh_pages
+git merge master
+git push origin gh_pages
+git checkout master
