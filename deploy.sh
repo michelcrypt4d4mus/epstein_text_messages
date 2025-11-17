fe4503db6065c6658e85f6764b64f8e46b3b7ccc
ashariyar <ashariyar@users.noreply.github.com>
2025-11-17 18:17:52 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+INDEX_HTML_PATH="docs/index.html"
+
 if any_uncommitted_changes; then
     echo "Uncommitted changes; halting."
     exit
+fi
+
+if [ -f "$INDEX_HTML_PATH" ]; then
+    echo "Removing '$INDEX_HTML_PATH' on master..."
+    rm "$INDEX_HTML_PATH"
 fi
 
 git checkout gh_pages
