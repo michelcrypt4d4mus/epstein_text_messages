1b91ab2d101cebeb7e4d641183c86e8abe677752
ashariyar <ashariyar@users.noreply.github.com>
2025-11-19 15:57:13 -0500

    code

--- util/env.py
+++ util/env.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from os import environ
 
 
@@ -5,3 +6,33 @@ deep_debug = len(environ.get('DEEP_DEBUG') or '') > 0
 is_debug = deep_debug or len(environ.get('DEBUG') or '') > 0
 
 include_redacted_emails = len(environ.get('EMAILS') or '') > 0
+
+
+#  of who is the counterparty in each file
+AI_COUNTERPARTY_DETERMINATION_TSV = StringIO("""
+filename	counterparty	source
+HOUSE_OVERSIGHT_025400.txt	Steve Bannon (likely)	Trump NYT article criticism; Hannity media strategy
+HOUSE_OVERSIGHT_025408.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_025452.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_025479.txt	Steve Bannon	China strategy and geopolitics; Trump discussions
+HOUSE_OVERSIGHT_025707.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_025734.txt	Steve Bannon	China strategy and geopolitics; Trump discussions
+HOUSE_OVERSIGHT_027260.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027281.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027346.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027365.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027374.txt	Steve Bannon	China strategy and geopolitics
+HOUSE_OVERSIGHT_027406.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027440.txt	Michael Wolff	Trump book/journalism project
+HOUSE_OVERSIGHT_027445.txt	Steve Bannon	China strategy and geopolitics; Trump discussions
+HOUSE_OVERSIGHT_027455.txt	Steve Bannon (likely)	China strategy and geopolitics; Trump discussions
+HOUSE_OVERSIGHT_027460.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027515.txt	Personal contact	Personal/social plans
+HOUSE_OVERSIGHT_027536.txt	Steve Bannon	China strategy and geopolitics; Trump discussions
+HOUSE_OVERSIGHT_027655.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027707.txt	Steve Bannon	Italian politics; Trump discussions
+HOUSE_OVERSIGHT_027722.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027735.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_027794.txt	Steve Bannon	Trump and New York Times coverage
+HOUSE_OVERSIGHT_029744.txt	Steve Bannon (likely)	Trump and New York Times coverage
+HOUSE_OVERSIGHT_031045.txt	Steve Bannon (likely)	Trump and New York Times coverage""".strip())
