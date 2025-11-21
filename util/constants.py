6b70b74dcdbdd9f8094a1d56e94d4dd1926e4519
ashariyar <ashariyar@users.noreply.github.com>
2025-11-21 14:53:52 -0500

    signature

--- util/constants.py
+++ util/constants.py
@@ -143,7 +143,6 @@ EMAILERS = [
     'Peter Aldhous',
     'Peter Green',
     'Robert Trivers',
-    'Sample, Elizabeth',
     'Steven Victor MD',
     'The Duke',
     'Tom Barrack',
@@ -275,16 +274,16 @@ KNOWN_RECIPIENTS = {
 EPSTEIN_SIGNATURE = re.compile(r"""please note
 The information contained in this communication is
 confidential, may be attorney-client privileged, may
-constitute inside information, and is intended only for
-the use of the addressee. It is the property of
+constitute inside information, and is intended only.*
+the use of the addressee. It is the property.*
 JEE
 Unauthorized use, disclosure or copying of this
 communication or any part thereof is strictly prohibited
 and may be unlawful. If you have received this
 communication in error, please notify us immediately by(\n\d\s*)?
-return e-mail or by e-mail to jeevacation.*gmail.com, and
-destroy this communication and all copies thereof,
-including all attachments. copyright -all rights reserved""")
+return e-mail or by e-mail to jeevacation.*
+destroy this communication and all copies thereo.*
+including all attachments. copyright -all rights reserved?""")
 
 EPSTEIN_OLD_SIGNATURE = re.compile(r"""\*+
 The information contained in this communication is
