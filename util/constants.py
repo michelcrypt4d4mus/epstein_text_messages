fa5865a526003ccd574161989ab7657a465465bd
ashariyar <ashariyar@users.noreply.github.com>
2025-11-21 23:13:09 -0500

    signature

--- util/constants.py
+++ util/constants.py
@@ -306,29 +306,7 @@ KNOWN_EMAIL_RECIPIENTS = {
     '019334': STEVE_BANNON,
 }
 
-
-EPSTEIN_SIGNATURE = re.compile(r"""(> )please note
-(> )?(• )?The information contained in this communication is
-(> )?confidential, may be attorney-client privileged, may
-(> )?constitute inside information, and is intended only.*
-(> )?the use of the addressee. It is the property.*(\nJEE|Jeffrey Epstein)?
-(> )?Unauthorized use, disclosure or copying of this
-(> )?communication or any part thereof is strictly prohibit.*
-(> )?and may be unlawful. If you have received this
-(> )?communication in error, please notify us immediately by(\n\d\s*)?
-(> )?return e-mail or by e-mail to.*
-(> )?destroy this communication and all copies thereo.*
-(> )?including all attachments. copyright -all rights reserved?""")
-
-EPSTEIN_OLD_SIGNATURE = re.compile(r"""\*+
-The information contained in this communication is
-confidential, may be attorney-client privileged, may
-constitute inside information, and is intend.*
-the use of the addressee. It is the property.*(\nJeffrey Epstein)?
-Unauthorized use, disclosure or copying of this
-communication or any part thereof is strictly prohibited
-and may be unlawful. If you have received this
-communication in error, please notify us immediately by
-return e-mail or by e-mail.*
-destroy this communication and all copies thereof.*
-including all attachments.( copyright -all rights reserved)?""")
+EPSTEIN_SIGNATURE = re.compile(
+    r"((\*+|please note)\n)?(> )?(• )?(» )?The information contained in this communication is\n(> )*(» )?confidential.*?all attachments.( copyright -all rights reserved?)?",
+    re.DOTALL
+)
