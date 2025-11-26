f6499c4374091ac87d3496dc5f0f791bf65f4c9b
ashariyar <ashariyar@users.noreply.github.com>
2025-11-26 02:20:28 -0500

    libra

--- util/rich.py
+++ util/rich.py
@@ -132,7 +132,7 @@ COUNTERPARTY_COLORS.update(OTHER_STYLES)
 
 HIGHLIGHT_PATTERNS: dict[str, str] = {
     ARAB_COLOR: r"Abu\s+Dhabi|Assad|Dubai|Emir(ates)?|Erdogan|Gaddafi|HBJ|Iran(ian)?|Islam(ic|ist)?|Istanbul|Kaz(akh|ich)stan|Kazakh?|KSA|MB(S|Z)|Mohammed\s+bin\s+Salman|Muslim|Saudi(\s+Arabian?)?|Sharia|Syria|Turkey|UAE|((Iraq|Iran|Kuwait|Qatar|Yemen)i?)",
-    BITCOIN_COLOR: r"bitcoin|coins|cr[iy]pto(currency)?|e-currency|(Howard\s+)?Lutnick|Tether",
+    BITCOIN_COLOR: r"bitcoin|coins|cr[iy]pto(currency)?|e-currency|(Howard\s+)?Lutnick|Libra|Tether",
     BRASIL_COLOR: r"Bra[sz]il(ian)?|Bolsonar[aio]|Lula",
     CHINA_COLOR: r"CCP|Chin(a|ese)|Guo|Kwok|Tai(pei|wan)|PRC|xi",
     INDIA_COLOR: r"Ambani|Indian?|Modi|mumbai",
