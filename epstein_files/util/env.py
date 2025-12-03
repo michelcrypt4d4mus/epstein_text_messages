09d9c96614622d0c18e2f6753dd341c2c85d1766
ashariyar <ashariyar@users.noreply.github.com>
2025-12-03 18:50:51 -0500

    width arg

--- epstein_files/util/env.py
+++ epstein_files/util/env.py
@@ -6,7 +6,8 @@ from sys import argv
 
 from rich.logging import RichHandler
 
-MAIN_SCRIPT = 'generate.py'
+DEFAULT_WIDTH = 120
+HTML_SCRIPTS = ['generate.py', 'count_words.py']
 
 
 parser = ArgumentParser(description="Parse epstein OCR docs and generate HTML page.")
@@ -20,6 +21,7 @@ parser.add_argument('--sort-alphabetical', '-alpha', action='store_true', help='
 parser.add_argument('--suppress-output', '-s', action='store_true', help='no output to terminal (use with --build)')
 parser.add_argument('--use-epstein-web-links', '-use', action='store_true', help='use epsteinweb.org links instead of epsteinify.com')
 parser.add_argument('--search-other', '-so', action='store_true', help='search for string in non email/text files (only used by search script)')
+parser.add_argument('--width', '-w', type=int, default=DEFAULT_WIDTH, help='screen width to use')
 parser.add_argument('--debug', '-d', action='store_true', help='set debug level to INFO')
 parser.add_argument('--deep-debug', '-dd', action='store_true', help='set debug level to DEBUG')
 parser.add_argument('--json-stats', action='store_true', help='print JSON formatted stats at the end')
@@ -31,8 +33,10 @@ is_env_var_set = lambda s: len(environ.get(s) or '') > 0
 deep_debug = args.deep_debug or is_env_var_set('DEEP_DEBUG')
 is_build = args.build or is_env_var_set('BUILD_HTML')
 is_debug = deep_debug or args.debug or is_env_var_set('DEBUG')
-is_main_script = Path(argv[0]).name == MAIN_SCRIPT
+is_html_script = Path(argv[0]).name in [HTML_SCRIPTS]
 skip_texts = args.no_texts or is_env_var_set('NO_TEXTS')
+
+args.width = args.width if is_html_script else None
 specified_emailers = args.emails or []
 
 
