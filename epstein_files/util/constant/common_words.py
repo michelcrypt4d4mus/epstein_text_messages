126cf0af271611b32c138ace62ed3ffadd28735f
ashariyar <ashariyar@users.noreply.github.com>
2025-12-04 15:03:55 -0500

    style

--- epstein_files/util/constant/common_words.py
+++ epstein_files/util/constant/common_words.py
@@ -2,156 +2,156 @@ from epstein_files.util.env import is_debug
 
 # https://www.gonaturalenglish.com/1000-most-common-words-in-the-english-language/
 MOST_COMMON_WORDS = """
-a
-about
-after
-all
-also
-am
-an
-and
-any
-are
-as
-at
-be
-because
-been
-but
-by
-came
-can
-can't
-come
-could
-couldnt
-day
-do
-dont
-did
-didnt
-even
-find
-first
-for
-from
-get
-got
-give
-go
-going
-had
-hadnt
-has
-hasnt
-have
-havent
-having
-he
-hed
-her
-here
-him
-his
-how
-i
-if
-in
-into
-is
-it
-its
-ive
-just
-know
-like
-look
-make
-man
-many
-me
-more
-my
-new
-no
-not
-now
-of
-on
-one
-only
-or
-other
-our
-out
-people
-pm
-re
-said
-say
-saying
-says
-see
-she
-shed
-so
-some
-subject
-take
-than
-that
-the
-their
-them
-then
-there
-these
-they
-theyll
-theyve
-thing
-think
-this
-those
-through
-time
-to
-too
-two
-up
-use
-very
-want
-was
-way
-we
-well
-went
-were
-what
-whatever
-when
-whenever
-where
-wherever
-which
-whichever
-who
-why
-will
-with
-without
-wont
-would
-wouldnt
-wouldve
-year
-you
-youd
-youll
-your
-youre
-youve
+    a
+    about
+    after
+    all
+    also
+    am
+    an
+    and
+    any
+    are
+    as
+    at
+    be
+    because
+    been
+    but
+    by
+    came
+    can
+    can't
+    come
+    could
+    couldnt
+    day
+    do
+    dont
+    did
+    didnt
+    even
+    find
+    first
+    for
+    from
+    get
+    got
+    give
+    go
+    going
+    had
+    hadnt
+    has
+    hasnt
+    have
+    havent
+    having
+    he
+    hed
+    her
+    here
+    him
+    his
+    how
+    i
+    if
+    in
+    into
+    is
+    it
+    its
+    ive
+    just
+    know
+    like
+    look
+    make
+    man
+    many
+    me
+    more
+    my
+    new
+    no
+    not
+    now
+    of
+    on
+    one
+    only
+    or
+    other
+    our
+    out
+    people
+    pm
+    re
+    said
+    say
+    saying
+    says
+    see
+    she
+    shed
+    so
+    some
+    subject
+    take
+    than
+    that
+    the
+    their
+    them
+    then
+    there
+    these
+    they
+    theyll
+    theyve
+    thing
+    think
+    this
+    those
+    through
+    time
+    to
+    too
+    two
+    up
+    use
+    very
+    want
+    was
+    way
+    we
+    well
+    went
+    were
+    what
+    whatever
+    when
+    whenever
+    where
+    wherever
+    which
+    whichever
+    who
+    why
+    will
+    with
+    without
+    wont
+    would
+    wouldnt
+    wouldve
+    year
+    you
+    youd
+    youll
+    your
+    youre
+    youve
 """.strip().split()
 
 OTHER_COMMON_WORDS = """
