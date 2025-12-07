74fefcd109542b78d5a07738e4adb23d06397a6a
ashariyar <ashariyar@users.noreply.github.com>
2025-12-07 18:33:35 -0500

    kluge

--- epstein_files/util/highlighted_group.py
+++ epstein_files/util/highlighted_group.py
@@ -115,7 +115,7 @@ HIGHLIGHTED_GROUPS = [
     HighlightedGroup(
         label='business',
         style='spring_green4',
-        pattern=r'Marc Rich|(Mi(chael|ke)\s*)?Ovitz|(Steve\s+)?Wynn|(Leslie\s+)?Wexner|Valhi',
+        pattern=r'(John\s*)?Kluge|Marc Rich|(Mi(chael|ke)\s*)?Ovitz|(Steve\s+)?Wynn|(Leslie\s+)?Wexner|Valhi',
         emailers = {
             ALIREZA_ITTIHADIEH: 'CEO Freestream Aircraft Limited',
             BARBRO_EHNBOM: 'Swedish pharmaceuticals',
