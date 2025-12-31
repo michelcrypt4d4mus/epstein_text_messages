1abf9974fd9099d31eb1de3a9acd73e66b7e25fe
ashariyar <ashariyar@users.noreply.github.com>
2025-12-31 11:21:42 -0500

    line shuffling

--- epstein_files/documents/email.py
+++ epstein_files/documents/email.py
@@ -513,10 +513,12 @@ class Email(CommunicationDocument):
             if self.file_id in ['030626']:  # Merge 6th and 7th (now 5th and 6th) rows
                 self._merge_lines(5)
         elif self.file_id == '029977':
-            self.text = self.text.replace('Sent 9/28/2012 2:41:02 PM', 'Sent: 9/28/2012 2:41:02 PM')
+            self._set_computed_fields(text=self.text.replace('Sent 9/28/2012 2:41:02 PM', 'Sent: 9/28/2012 2:41:02 PM'))
 
             for i in range(4):
                 self._merge_lines(2)
+
+            lines = self.lines[0:2] + [self.lines[2] + self.lines[4]] + [self.lines[3]] + self.lines[5:]
 
         if old_text != self.text:
             logger.warning(f"Modified {self.url_slug} text, old:\n\n" + '\n'.join(old_text.split('\n')[0:12]) + '\n')
