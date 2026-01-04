3e29bcba614d802462a164e73ea12f8e88cc1aea
ashariyar <ashariyar@users.noreply.github.com>
2026-01-04 15:46:58 -0500

    remove cruft

--- tests/util/test_data.py
+++ tests/util/test_data.py
@@ -2,5 +2,6 @@ from epstein_files.util.data import extract_last_name
 
 
 def test_extract_last_name():
+    assert extract_last_name("Thomas") == "Thomas"
     assert extract_last_name("Thomas Landon") == "Landon"
     assert extract_last_name("Thomas Landon Jr.") == "Landon Jr."
