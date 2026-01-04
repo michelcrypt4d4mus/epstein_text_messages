6d9ff2430e05731f1834aa2fd3c4ee6717b80525
ashariyar <ashariyar@users.noreply.github.com>
2026-01-04 02:07:48 -0500

    024624 actual text

--- scripts/show_file_contents.py
+++ scripts/show_file_contents.py
@@ -5,6 +5,7 @@ import sys
 from rich.panel import Panel
 
 from scripts.use_pickled import epstein_files
+from epstein_files.documents.email import Email
 from epstein_files.util.env import args
 from epstein_files.util.file_helper import extract_file_id, id_str
 from epstein_files.util.rich import console, print_panel, print_section_header
@@ -22,5 +23,10 @@ for doc in docs:
     console.line()
     console.print(doc)
     console.line()
-    console.print(Panel(f"*** RAW {doc.url_slug} ***", expand=False))
+    console.print(Panel(f"*** {doc.url_slug} RAW ***", expand=False))
     console.print(doc.raw_text())
+
+    if isinstance(doc, Email):
+        console.line()
+        console.print(Panel(f"*** {doc.url_slug} actual_text ***", expand=False))
+        console.print(doc._actual_text())
