cae8f9d54064b0d051ee04a60f03200c89f943d2
ashariyar <ashariyar@users.noreply.github.com>
2026-01-05 15:11:07 -0500

    tests

--- tests/test_other_files.py
+++ tests/test_other_files.py
@@ -4,3 +4,11 @@ from epstein_files.documents.document import Document
 def test_other_files_author_count(epstein_files):
     assert Document.known_author_count(epstein_files.other_files) == 353
     assert len(epstein_files.json_files) == 19
+
+
+def test_other_files_categories(epstein_files):
+    assert len([f for f in epstein_files.other_files if f.category() is None]) == 0
+
+
+def test_interesting_file_count(epstein_files):
+    assert len([f.file_id for f in epstein_files.other_files if f.is_interesting()]) == 196
