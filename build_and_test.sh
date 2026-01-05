44c751fc9e6fbcc48f3bd16ef5656fe4f0aa26e0
ashariyar <ashariyar@users.noreply.github.com>
2026-01-04 23:41:20 -0500

    readme

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build a new pickle file and run pytest against it
 
-./generate_html.py --build --suppress-output --overwrite-pickle
+epstein_generate --build --suppress-output --overwrite-pickle
 echo -e "\nDone building, launching pytest...\n\n"
 PICKLED=true pytest
