19ee8f8f8bd671ebbb00438600856b8d95d1a09e
ashariyar <ashariyar@users.noreply.github.com>
2026-01-05 21:06:33 -0500

    style

--- epstein_files/util/constant/names.py
+++ epstein_files/util/constant/names.py
@@ -178,6 +178,7 @@ JARED_KUSHNER = 'Jared Kushner'
 JULIE_K_BROWN = 'Julie K. Brown'
 KARIM_SADJADPOUR = 'KARIM SADJADPOUR'.title()
 MICHAEL_J_BOCCIO = 'Michael J. Boccio'
+NERIO_ALESSANDRI = 'Nerio Alessandri (Founder and Chairman of Technogym S.p.A. Italy)'
 PAUL_G_CASSELL = 'Paul G. Cassell'
 RUDY_GIULIANI = 'Rudy Giuliani'
 TULSI_GABBARD = 'Tulsi Gabbard'
