60d415801657a964e3251ce6e7ab0969079113af
ashariyar <ashariyar@users.noreply.github.com>
2026-01-05 20:42:00 -0500

    Remove category for EmailCft and TextCfg

--- epstein_files/util/doc_cfg.py
+++ epstein_files/util/doc_cfg.py
@@ -229,10 +229,6 @@ class EmailCfg(CommunicationCfg):
     is_fwded_article: bool = False
     recipients: list[str | None] = field(default_factory=list)
 
-    def __post_init__(self):
-        super().__post_init__()
-        self.category = EMAIL
-
     @classmethod
     def from_doc_cfg(cls, cfg: DocCfg) -> 'EmailCfg':
         return cls(**asdict(cfg))
@@ -244,10 +240,6 @@ class EmailCfg(CommunicationCfg):
 
 @dataclass(kw_only=True)
 class TextCfg(CommunicationCfg):
-    def __post_init__(self):
-        super().__post_init__()
-        self.category = TEXT_MESSAGE
-
     # This is necessary because for some dumb reason @dataclass(repr=False) doesn't cut it
     def __repr__(self) -> str:
         return super().__repr__()
