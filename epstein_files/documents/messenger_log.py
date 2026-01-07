803b60d7d17588e9447f0eacf05182353f0193c9
ashariyar <ashariyar@users.noreply.github.com>
2026-01-07 00:20:24 -0500

    imports

--- epstein_files/documents/messenger_log.py
+++ epstein_files/documents/messenger_log.py
@@ -12,7 +12,7 @@ from epstein_files.documents.communication import Communication
 from epstein_files.documents.imessage.text_message import TextMessage
 from epstein_files.util.constant.names import JEFFREY_EPSTEIN, UNKNOWN
 from epstein_files.util.constant.strings import AUTHOR, TIMESTAMP_STYLE
-from epstein_files.util.data import days_between, days_between_str, iso_timestamp, listify, sort_dict
+from epstein_files.util.data import days_between, days_between_str, iso_timestamp, sort_dict
 from epstein_files.util.doc_cfg import Metadata, TextCfg
 from epstein_files.util.highlighted_group import get_style_for_name
 from epstein_files.util.logging import logger
