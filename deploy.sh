fec5ed27a355f8bda778d502e78a193ddd502bd5
ashariyar <ashariyar@users.noreply.github.com>
2026-01-07 15:32:26 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -67,6 +67,9 @@ else
 fi
 
 echo -e ""
+print_msg "Building chronological emails page..."
+epstein_generate --build --email-timeline --suppress-output
+echo -e ""
 print_msg "Building word counts page..."
 epstein_word_count --build --suppress-output --width 125
 echo -e ""
