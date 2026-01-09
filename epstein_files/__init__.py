0aab17af968c37f7dde3ab211e02fc7a97edd93d
ashariyar <ashariyar@users.noreply.github.com>
2026-01-09 18:42:32 -0500

    throw error on no file

--- epstein_files/__init__.py
+++ epstein_files/__init__.py
@@ -115,10 +115,15 @@ def epstein_search():
 def epstein_show():
     """Show the color highlighted file. If --raw arg is passed, show the raw text of the file as well."""
     _assert_positional_args()
-    ids = [extract_file_id(arg) for arg in args.positional_args]
-    raw_docs = [Document(coerce_file_path(id)) for id in ids]
-    docs = Document.sort_by_timestamp([document_cls(doc)(doc.file_path) for doc in raw_docs])
+    raw_docs: list[Document] = []
     console.line()
+
+    try:
+        ids = [extract_file_id(arg) for arg in args.positional_args]
+        raw_docs = [Document(coerce_file_path(id)) for id in ids]
+        docs = Document.sort_by_timestamp([document_cls(doc)(doc.file_path) for doc in raw_docs])
+    except Exception as e:
+        exit_with_error(str(e))
 
     for doc in docs:
         console.print('\n', doc, '\n')
