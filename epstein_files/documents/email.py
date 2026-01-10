65a47a1d79e1ee709bc9de2a1e4bfd9735fd7703
ashariyar <ashariyar@users.noreply.github.com>
2026-01-09 21:23:49 -0500

    sort recipients

--- epstein_files/documents/email.py
+++ epstein_files/documents/email.py
@@ -400,7 +400,7 @@ class Email(Communication):
         if not self.is_note_to_self():
             self.recipients = [r for r in self.recipients if r != self.author]
 
-        self.recipients = list(set(self.recipients))
+        self.recipients = list(set(sorted(self.recipients, key=lambda r: r or UNKNOWN)))
         self.text = self._prettify_text()
         self.actual_text = self._actual_text()
         self.sent_from_device = self._sent_from_device()
