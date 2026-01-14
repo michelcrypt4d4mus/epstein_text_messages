57f92fd815c97315e9cfe631f52dfc35df6bea09
ashariyar <ashariyar@users.noreply.github.com>
2026-01-14 00:01:15 -0500

    hakeem

--- epstein_files/util/output.py
+++ epstein_files/util/output.py
@@ -56,7 +56,6 @@ DEFAULT_EMAILERS = [
 INTERESTING_EMAIL_IDS = [
     '032229',  # Michael Wolff on strategy
     '028784',  # seminars: Money / Power
-    '029342',  # Hakeem Jeffries
     '030630',  # 'What happens with zubair's project?'
     '033178',  # 'How is it going with Zubair?'
     '022396',  # Ukraine friend
