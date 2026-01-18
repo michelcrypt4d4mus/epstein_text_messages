04c8ae721e9220969eb90c8087cb9640c4eabb1e
ashariyar <ashariyar@users.noreply.github.com>
2026-01-18 15:16:57 -0500

    use inkscape instead of cairosvg (with fallback) for .png generation

--- epstein_files/util/output.py
+++ epstein_files/util/output.py
@@ -86,17 +86,20 @@ def print_emailers_info(epstein_files: EpsteinFiles) -> None:
     svg_path = f"{EMAILERS_TABLE_PNG_PATH}.svg"
     console.save_svg(svg_path, theme=HTML_TERMINAL_THEME, title="Epstein Emailers")
     log_file_write(svg_path)
-    import cairosvg
-    cairosvg.svg2png(url=svg_path, write_to=str(EMAILERS_TABLE_PNG_PATH))
-    log_file_write(EMAILERS_TABLE_PNG_PATH)
 
     # Inkscape is better at converting svg to png
-    inkscape_png_path = f"{EMAILERS_TABLE_PNG_PATH}.inkscape.png"
-    inkscape_cmd_args = ['inkscape', f'--export-filename={inkscape_png_path}', svg_path]
+    inkscape_cmd_args = ['inkscape', f'--export-filename={EMAILERS_TABLE_PNG_PATH}', svg_path]
     logger.warning(f"Running inkscape cmd: {' '.join(inkscape_cmd_args)}")
-    check_output(inkscape_cmd_args)
-    log_file_write(inkscape_png_path)
-    # unlink(svg_path)
+
+    try:
+        check_output(inkscape_cmd_args)
+    except CalledProcessError as e:
+        logger.error(f"Failed to convert svg to png with inkscape, falling back to cairosvg: {e}")
+        import cairosvg
+        cairosvg.svg2png(url=svg_path, write_to=str(EMAILERS_TABLE_PNG_PATH))
+
+    log_file_write(EMAILERS_TABLE_PNG_PATH)
+    unlink(svg_path)
 
 
 def print_emails_section(epstein_files: EpsteinFiles) -> list[Email]:
