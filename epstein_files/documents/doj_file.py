080aac7c3958e78c594cb49bcc8e9550da18971e
ashariyar <ashariyar@users.noreply.github.com>
2026-02-02 21:47:18 -0500

    allow dateutil.parser to use fuzzy matching for email datetimes

--- epstein_files/documents/doj_file.py
+++ epstein_files/documents/doj_file.py
@@ -195,7 +195,11 @@ class DojFile(OtherFile):
     def printable_doc(self) -> Self | Email:
         """Return a copy of this `DojFile` with simplified text if file ID is in `REPLACEMENT_TEXT`."""
         if Document.is_email(self):
-            return Email(self.file_path, text=self.text)  # Pass text= to avoid reprocessing
+            try:
+                return Email(self.file_path, text=self.text)  # Pass text= to avoid reprocessing
+            except Exception as e:
+                self.warn(f"Error creating Email object, trying full reload of text...")
+                return Email(self.file_path)
         else:
             return self
 
