29cbe6af022c0cae3e7a8b251147e6afa223d0e0
ashariyar <ashariyar@users.noreply.github.com>
2026-02-11 01:15:22 -0500

    repair Sent and Subject without colon

--- epstein_files/documents/email.py
+++ epstein_files/documents/email.py
@@ -65,7 +65,7 @@ OCR_REPAIRS: dict[str | re.Pattern, str] = {
     re.compile(r"([<>.=_HIM][<>.=_HIM14]{5,}[<>.=_HIM]|MOMMINNEMUMMIN) *(wrote:?)?"): rf"{REDACTED} \2",
     re.compile(r"([,<>_]|AM|PM)\n(>)? ?wrote:?"): r'\1\2 wrote:',
     # Headers
-    re.compile(r"^(Sent|Subject) ", re.MULTILINE): r'\1:',
+    re.compile(r"^(Sent|Subject) (?![Ff]rom)", re.MULTILINE): r'\1:',
     # Names / email addresses
     'Alireza lttihadieh': ALIREZA_ITTIHADIEH,
     'Miroslav Laj6ak': MIROSLAV_LAJCAK,
