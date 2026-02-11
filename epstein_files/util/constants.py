a44d0240655e1a6a53d2caef9c429dd5f41f364e
ashariyar <ashariyar@users.noreply.github.com>
2026-02-11 12:09:38 -0500

    constants

--- epstein_files/util/constants.py
+++ epstein_files/util/constants.py
@@ -1261,6 +1261,7 @@ EMAILS_CONFIG = [
         description=f"forged email Epstein wrote to complain about the Mindshift conference to {AL_SECKEL}",
         is_interesting=True,
     ),
+    EmailCfg(id='EFTA01007544', description=f'core bitcoin dev {JEREMY_RUBIN} says "there\'s a grey area between pump and develop"', is_interesting=True),
     EmailCfg(id='EFTA01784901', description=f"discussion of checks {JEREMY_RUBIN} has cashed from Epstein", is_interesting=True),
     EmailCfg(id='EFTA00699275', description=f"'Fred' is probably Coinbase co-founder Fred Ehrsam"),
     EmailCfg(id='EFTA01013266', description=f'request for payment to {CRYPTO_PR_LAB}', is_interesting=True),
