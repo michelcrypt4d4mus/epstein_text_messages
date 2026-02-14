2c640829d9bfdf94c4c86ef35d2818a48772e882
ashariyar <ashariyar@users.noreply.github.com>
2026-02-14 12:49:15 -0500

    move HighlightedNames classes to their own file

--- epstein_files/util/highlighted_group.py
+++ epstein_files/util/highlighted_group.py
@@ -1,20 +1,19 @@
-import json
 import re
 from collections import defaultdict
-from dataclasses import dataclass, field
 
 from rich.console import Console
 from rich.highlighter import RegexHighlighter
 from rich.text import Text
 
 from epstein_files.documents.documents.doc_cfg import *
+from epstein_files.output.highlighted_names import REGEX_STYLE_PREFIX, HighlightedNames, HighlightedText, ManualHighlight
 from epstein_files.people.contact_info import ContactInfo
 from epstein_files.util.constant.names import *
 from epstein_files.util.constant.strings import *
 from epstein_files.util.constant.urls import ARCHIVE_LINK_COLOR
 from epstein_files.util.constants import EPSTEIN_V_ROTHSTEIN_EDWARDS, REPLY_REGEX, SENT_FROM_REGEX
 from epstein_files.util.env import args
-from epstein_files.util.helpers.data_helpers import sort_dict, without_falsey
+from epstein_files.util.helpers.data_helpers import sort_dict
 from epstein_files.util.helpers.string_helper import indented
 from epstein_files.util.logging import logger
 
@@ -28,7 +27,6 @@ EPSTEIN_ESTATE_EXECUTOR = f"Epstein {ESTATE_EXECUTOR}"
 MC2_MODEL_MANAGEMENT = f"{JEAN_LUC_BRUNEL}'s MC2 Model Management"
 MIDEAST = 'mideast'
 QUESTION_MARKS_TXT = Text(QUESTION_MARKS, style='grey50')
-REGEX_STYLE_PREFIX = 'regex'
 TECH_BRO = 'tech bro'
 VICTIM_COLOR = 'orchid1'
 
@@ -47,152 +45,6 @@ CATEGORY_STYLES = {
 }
 
 debug_console = Console(color_system='256')
-
-
-@dataclass(kw_only=True)
-class BaseHighlight:
-    """
-    Regex and style information for things we want to highlight.
-
-    Attributes:
-        label (str): RegexHighlighter match group name
-        regex (re.Pattern): regex pattern identifying strings matching this group
-        style (str): Rich style to apply to text matching this group
-        theme_style_name (str): The style name that must be a part of the rich.Console's theme
-    """
-    label: str = ''
-    regex: re.Pattern = field(init=False)
-    style: str
-    theme_style_name: str = field(init=False)
-    _capture_group_label: str = field(init=False)
-    _match_group_var: str = field(init=False)
-
-    def __post_init__(self):
-        if not self.label:
-            raise ValueError(f'Missing label for {self}')
-
-        self._capture_group_label = self.label.lower().replace(' ', '_').replace('-', '_')
-        self._match_group_var = fr"?P<{self._capture_group_label}>"
-        self.theme_style_name = f"{REGEX_STYLE_PREFIX}.{self._capture_group_label}"
-
-
-@dataclass(kw_only=True)
-class HighlightedText(BaseHighlight):
-    """
-    Color highlighting for things other than people's names (e.g. phone numbers, email headers).
-
-    Attributes:
-        patterns (list[str]): regex patterns identifying strings matching this group
-    """
-    patterns: list[str] = field(default_factory=list)
-    _pattern: str = field(init=False)
-
-    def __post_init__(self):
-        super().__post_init__()
-
-        if not self.label:
-            raise ValueError(f"No label provided for {repr(self)}")
-
-        self._pattern = '|'.join(self.patterns)
-        self.regex = re.compile(fr"({self._match_group_var}{self._pattern})", re.IGNORECASE | re.MULTILINE)
-
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}(label='{self.label}', pattern='{self._pattern}', style='{self.style}')"
-
-
-@dataclass(kw_only=True)
-class HighlightedNames(HighlightedText):
-    """
-    Encapsulates info about people, places, and other strings we want to highlight with RegexHighlighter.
-    Constructor must be called with either an 'emailers' arg or a 'pattern' arg (or both).
-
-    Attributes:
-        category (str): optional string to use as an override for self.label in some contexts
-        contacts (list[ContactInfo]): optional `ContactInfo` objects with names and regexes
-        contacts_lookup (dict[Name, ContactInfo]): lookup dictionary for `ContactInfo` objects
-        should_match_first_last_name (bool): if False don't match first/last/reversed versions of emailers
-        _pattern (str): regex pattern combining 'pattern' with first & last names of all 'emailers'
-    """
-    category: str = ''
-    contacts: list[ContactInfo] = field(default_factory=list)
-    contacts_lookup: dict[Name, ContactInfo] = field(default_factory=dict)
-    should_match_first_last_name: bool = True  # TODO: this no longer does anything?
-
-    def __post_init__(self):
-        if not (self.patterns or self.contacts):
-            raise ValueError(f"Must provide either 'emailers' or 'pattern' arg.")
-        elif not self.label:
-            if len(self.contacts) == 1 and self.contacts[0].name:
-                self.label = self.contacts[0].name
-            else:
-                raise ValueError(f"No label provided for {repr(self)}")
-
-        super().__post_init__()
-        self._pattern = '|'.join([contact.highlight_pattern for contact in self.contacts] + self.patterns)
-        self.regex = re.compile(fr"\b({self._match_group_var}({self._pattern})s?)\b", re.IGNORECASE)
-        self.contacts_lookup = ContactInfo.build_name_lookup(self.contacts)
-
-    def category_str(self) -> str:
-        if self.category:
-            return self.category
-        elif len(self.contacts) == 1 and self.label == self.contacts[0].name:
-            return ''
-        else:
-            return self.label.replace('_', ' ')
-
-    def info_for(self, name: str, include_category: bool = False) -> str | None:
-        """Label and additional info for 'name' if 'name' is in `self.contacts`."""
-        info_pieces = [self.category_str()] if include_category else []
-
-        if (contact := self.contacts_lookup.get(name)):
-            info_pieces.append(contact.info)
-
-        info_pieces = without_falsey(info_pieces)
-        return ', '.join(info_pieces) if info_pieces else None
-
-    def __str__(self) -> str:
-        return super().__str__()
-
-    def __repr__(self) -> str:
-        s = f"{type(self).__name__}("
-
-        for property in ['label', 'style', 'category', 'patterns', 'contacts']:
-            value = getattr(self, property)
-
-            if not value or (property == 'label' and len(self.contacts) == 1 and not self.patterns):
-                continue
-
-            s += f"\n    {property}="
-
-            if isinstance(value, dict):
-                s += '{'
-
-                for k, v in value.items():
-                    s += f"\n        {constantize_name(k)}: {json.dumps(v).replace('null', 'None')},"
-
-                s += '\n    },'
-            elif property == 'patterns':
-                s += '[\n        '
-                s += repr(value).removeprefix('[').removesuffix(']').replace(', ', ',\n        ')
-                s += ',\n    ],'
-            else:
-                s += f"{json.dumps(value)},"
-
-        return s + '\n)'
-
-
-@dataclass(kw_only=True)
-class ManualHighlight(BaseHighlight):
-    """For when you can't construct the regex."""
-    pattern: str
-
-    def __post_init__(self):
-        super().__post_init__()
-
-        if self._match_group_var not in self.pattern:
-            raise ValueError(f"Label '{self.label}' must appear in regex pattern '{self.pattern}'")
-
-        self.regex = re.compile(self.pattern, re.MULTILINE)
 
 
 HIGHLIGHTED_NAMES: list[HighlightedNames | HighlightedText | ManualHighlight] = [
