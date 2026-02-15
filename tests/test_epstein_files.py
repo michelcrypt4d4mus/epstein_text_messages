0e0ad60f09337f5c43330ac42d9d1c99dd71926c
ashariyar <ashariyar@users.noreply.github.com>
2026-02-15 18:19:26 -0500

    consolidate test_epstein_files

--- tests/test_epstein_files.py
+++ tests/test_epstein_files.py
@@ -1,14 +1,23 @@
 from datetime import datetime
 
-from epstein_files.documents.document import Document
-from epstein_files.documents.other_file import OtherFile
+import pytest
 
+from epstein_files.documents.document import Document
+from epstein_files.documents.email import Email
+from epstein_files.documents.other_file import OtherFile
 from epstein_files.documents.messenger_log import MessengerLog
 from epstein_files.epstein_files import count_by_month
 from epstein_files.output.rich import console
 from epstein_files.util.constant.names import *
 from epstein_files.util.constants import CONFIGS_BY_ID
 
+from epstein_files.util.constant.names import *
+from epstein_files.util.helpers.data_helpers import dict_sets_to_lists
+
+from .fixtures.emails.email_author_counts import EMAIL_AUTHOR_COUNTS
+from .fixtures.emails.email_recipient_counts import EMAIL_RECIPIENT_COUNTS
+from .fixtures.emails.signatures import AUTHORS_TO_DEVICE_SIGNATURES, DEVICE_SIGNATURE_TO_AUTHORS, SIGNATURE_SUBSTITUTION_COUNTS
+from .fixtures.emails.unknown_recipient_file_ids import UNKNOWN_RECIPIENT_FILE_IDS
 from .fixtures.file_counts_by_month import EXPECTED_MONTHLY_COUNTS
 from .fixtures.messenger_logs.author_counts import MESSENGER_LOG_AUTHOR_COUNTS
 from .fixtures.other_files.interesting_file_ids import INTERESTING_FILE_IDS
@@ -54,3 +63,41 @@ def test_other_files_author_count(epstein_files):
 
 def test_other_files_categories(epstein_files):
     assert len([f for f in epstein_files.other_files if not f.category]) == 2425
+
+
+################################################
+#################### EMAILS ####################
+################################################
+
+# @pytest.mark.skip(reason='temporary')
+def test_email_author_counts(epstein_files):
+    author_counts = epstein_files.email_author_counts()
+    assert author_counts.pop(JEFFREY_EPSTEIN) > 790
+    assert author_counts.pop(LESLEY_GROFF) > 100
+    assert author_counts.pop(RICHARD_KAHN) > 50
+    assert author_counts == EMAIL_AUTHOR_COUNTS
+
+
+# @pytest.mark.skip(reason='temporary')
+def test_email_recipient_counts(epstein_files):
+    recipient_counts = epstein_files.email_recipient_counts()
+    assert recipient_counts.pop(JEFFREY_EPSTEIN) > 1800
+    assert recipient_counts.pop(LESLEY_GROFF) > 40
+    assert recipient_counts.pop(RICHARD_KAHN) > 50
+    assert recipient_counts == EMAIL_RECIPIENT_COUNTS
+
+
+def test_signature_substitutions(epstein_files):
+    substitution_counts = epstein_files.email_signature_substitution_counts()
+    # epstein_substitutions = substitution_counts.pop(JEFFREY_EPSTEIN)
+    # assert epstein_substitutions > 3840
+    assert substitution_counts == SIGNATURE_SUBSTITUTION_COUNTS
+
+
+def test_signatures(epstein_files):
+    assert dict_sets_to_lists(epstein_files.email_authors_to_device_signatures()) == AUTHORS_TO_DEVICE_SIGNATURES
+    assert dict_sets_to_lists(epstein_files.email_device_signatures_to_authors()) == DEVICE_SIGNATURE_TO_AUTHORS
+
+
+def test_unknown_recipient_file_ids(epstein_files):
+    assert epstein_files.unknown_recipient_ids() == UNKNOWN_RECIPIENT_FILE_IDS
