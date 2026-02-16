e703d9b341d4cb47b176809246f49feb25e3b135
ashariyar <ashariyar@users.noreply.github.com>
2026-02-15 22:05:52 -0500

    fix

--- epstein_files/util/helpers/debugging_helper.py
+++ epstein_files/util/helpers/debugging_helper.py
@@ -16,7 +16,7 @@ def _show_timestamps(epstein_files):
 def _verify_filenames(epstein_files):
     doc_filenames = set([doc.file_path.name for doc in epstein_files.all_documents])
 
-    for file_path in epstein_files.all_files:
+    for file_path in epstein_files.file_paths:
         if file_path.name not in doc_filenames:
             print(f"'{file_path}' is not in list of {len(doc_filenames)} Document obj filenames!")
 
