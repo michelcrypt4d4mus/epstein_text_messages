5c5717d216fd96a0f1f7be2f76609e6d385719c4
ashariyar <ashariyar@users.noreply.github.com>
2026-02-17 05:05:47 -0500

    don't commit it

--- deploy.sh
+++ deploy.sh
@@ -58,9 +58,9 @@ print_deploy_step "Building emailer info .png... $PICKLE_ARG"
 $GENERATE_CMD --emailers-info $PICKLE_ARG
 
 # Copy pickle file to emails_extracted_from_legal_filings/
-if [[ $PICKLE_ARG == "--overwrite-pickle" ]]; then
-    cp ./the_epstein_files.pkl.gz emails_extracted_from_legal_filings/
-fi
+# if [[ $PICKLE_ARG == "--overwrite-pickle" ]]; then
+#     cp ./the_epstein_files.pkl.gz emails_extracted_from_legal_filings/
+# fi
 
 # Commit if any changes
 if any_uncommitted_changes; then
