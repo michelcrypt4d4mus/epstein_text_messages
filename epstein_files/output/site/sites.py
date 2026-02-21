d229ff3975be5cdec0a7aa518d814569e2068fe4
ashariyar <ashariyar@users.noreply.github.com>
2026-02-21 02:03:49 -0500

    emails

--- epstein_files/output/site/sites.py
+++ epstein_files/output/site/sites.py
@@ -132,8 +132,8 @@ HTML_BUILD_FILENAMES = {
 SITE_DESCRIPTIONS = {
     SiteType.CURATED:               f"curated:by my interests, files grouped by type",
     SiteType.CHRONOLOGICAL:         f"curated chronological:all types intermingled",
-    SiteType.EMAILS:                f"emailers:emails grouped by counterparty",
-    SiteType.EMAILS_CHRONOLOGICAL:  f"emails:pure chronological order",
+    SiteType.EMAILS:                f"emailers:ALL emails grouped by counterparty",
+    SiteType.EMAILS_CHRONOLOGICAL:  f"emails:ALL emails chronological order",
     SiteType.TEXT_MESSAGES:         f"text messages:{HOUSE_OVERSIGHT_TRANCHE}",
     SiteType.CURATED_MOBILE:        f"mobile:an attempt at mobile compatibility",
     SiteType.CHRONOLOGICAL_MOBILE:  f"chrono mobile:another attempt at mobile compatibility",
