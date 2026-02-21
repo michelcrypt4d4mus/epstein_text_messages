8fa4cc956e5750f00b509a801bcdfc4c414ff6a8
ashariyar <ashariyar@users.noreply.github.com>
2026-02-21 01:51:54 -0500

    undo mobile limitations

--- epstein_files/people/person.py
+++ epstein_files/people/person.py
@@ -415,8 +415,8 @@ class Person:
         grey_idx = 0
 
         # TODO: iPhone simulator seems to barf on the big table
-        if args.mobile:
-            people = highlighted[0:3]
+        # if args.mobile:
+        #     people = highlighted[0:3]
 
         for person in people:
             if person.is_uninteresting and not (args.emailers_info or args.all_emails):
