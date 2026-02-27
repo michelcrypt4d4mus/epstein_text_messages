63eceefc021bcad94535a98133b3f348dfc69947
ashariyar <ashariyar@users.noreply.github.com>
2026-02-27 13:11:43 -0500

    companies

--- epstein_files/util/constant/names.py
+++ epstein_files/util/constant/names.py
@@ -340,7 +340,7 @@ NAMES_TO_NOT_PARTIALLY_MATCH = set("""
     oleg owen
     paul paula pen perry peter philip pierce plus police prince
     randall rangel reid richard robert roberts rodriguez roger rosenberg ross roth roy rubenstein rubin
-    santos scott sean sergey service services skip smith stacey stanley stern stephen steve steven stone story susan
+    santos scott sean sergey service services skip smith son stacey stanley stern stephen steve steven stone story susan
     team terry the thomas tim tom tony trust tyler
     unknown
     ventures victor viktor virginia
