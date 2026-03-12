ac283d8ba10f8204d7e7546c836757000cd7173c
ashariyar <ashariyar@users.noreply.github.com>
2026-03-11 23:07:05 -0400

    comment

--- epstein_files/documents/document.py
+++ epstein_files/documents/document.py
@@ -298,11 +298,10 @@ class Document:
 
     @property
     def panel_title_timestamp(self) -> str:
-        """String placed in the `title` of the enclosing `Panel` when printing this document's text."""
+        """Time or date string placed in the `title` of the enclosing `Panel` when printing this `Document`."""
         if self.timestamp is None or self.timestamp == FALLBACK_TIMESTAMP:
             return ''
-
-        if self.config and self.config.timestamp:
+        elif self.config and self.config.timestamp:
             if self.config.date_uncertain:
                 prefix = 'approximate'
             else:
