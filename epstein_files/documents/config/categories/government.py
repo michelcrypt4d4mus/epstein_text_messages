f865343162a87923472b36baf89348bd309404d3
ashariyar <ashariyar@users.noreply.github.com>
2026-03-13 07:35:18 -0400

    cfg

--- epstein_files/documents/config/categories/government.py
+++ epstein_files/documents/config/categories/government.py
@@ -43,6 +43,7 @@ GOVERNMENT_CFGS = [
     DocCfg(id='EFTA00039312', author=BUREAU_OF_PRISONS, replace_text_with='Program Statement / Memo about BOP Pharmacy Program'),
     DocCfg(id='EFTA00039351', author=BUREAU_OF_PRISONS, replace_text_with='Program Statement / Memo about BOP Pharmacy Program'),
     DocCfg(id='EFTA00039156', author=BUREAU_OF_PRISONS, replace_text_with='Standards of Employee Conduct'),
+    DocCfg(id='EFTA01125108', author='DHS', description=f'receipt for I129 Petition for a Nonimmigrant Worker filed by Sublime Art LLC, {ARDA_BESKARDES}', is_interesting=True),
     DocCfg(
         id='EFTA00164939',
         author=DOJ,
@@ -81,6 +82,7 @@ GOVERNMENT_CFGS = [
         description='tip about recently convicted rapists Tal and Oren Alexander at Epstein\'s house',
         show_full_panel=True,
     ),
+    DocCfg(id='EFTA00151754', author=FBI, description='delaration of Law Enforcement Officer for Victim of Trafficking', is_interesting=True),
     DocCfg(id='EFTA00222943', author=FBI, description=f"agent believes computers were removed from Epstein's residence"),
     DocCfg(id='EFTA00020506', author=FBI, description='"chauffeur told Epstein \'I have something on you, remember what I buried!\'"', show_full_panel=True),
     DocCfg(id='EFTA01249591', author=FBI, description=f"allegations against {HENRY_JARECKI}"),
@@ -91,7 +93,7 @@ GOVERNMENT_CFGS = [
     DocCfg(id='EFTA00101927', author=FBI, description=f'interview with someone who mentions au pair of Glenn and {EVA_DUBIN} held against her will'),
     DocCfg(id='EFTA00173569', author=FBI, description='report on hack of FBI Epstein files repo by foreign actor', is_interesting=True),
     DocCfg(id='EFTA00247131', author=FBI, description='search warrant for New York house', date='2019-07-07'),
-    DocCfg(id='EFTA01731217', author=FBI, description=f'requesting INS allow {NADIA_MARCINKO} be allowed to stay in the US because of an ongoing sex-trafficking case', is_Interesting=True),
+    DocCfg(id='EFTA01731217', author=FBI, description=f'requesting INS allow {NADIA_MARCINKO} be allowed to stay in the US because of an ongoing sex-trafficking case', is_interesting=True),
     DocCfg(id='EFTA00084614', author=PALM_BEACH_POLICE, description='incident report detailing the investigation into Jeffrey Epstein'),
     DocCfg(id='EFTA00007893', author=PALM_BEACH_POLICE, description=f"receipts, notes, bank statements of {GHISLAINE_MAXWELL}"),
     DocCfg(id='EFTA00005569', author=PALM_BEACH_POLICE, replace_text_with='photo lineup featuring Epstein', date='2005-03-17'),
@@ -105,6 +107,7 @@ GOVERNMENT_CFGS = [
     fbi_defense_witness('EFTA02730267', 'Malcolm Grumbridge', '2022-04-14'),
     fbi_defense_witness('EFTA02730271', REDACTED, '2022-03-22'),
     fbi_defense_witness('EFTA02730477', 'Roderic Alexander', '2022-01-19'),
+    letter('EFTA01653121', FBI, ['USCIS'], "regarding an individual's cooperation in the investigation of Epstein and Maxwell"),
     letter('EFTA00098456', PAUL_G_CASSELL, ['Scotland Yard'], 'International Sex Trafficking by Jeffrey Epstein, contains court filings'),
 
     # Emails
