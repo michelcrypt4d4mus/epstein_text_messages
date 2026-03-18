f0495c241e99b8dafeee716e39003fdbb7178878
ashariyar <ashariyar@users.noreply.github.com>
2026-03-18 03:06:11 -0400

    ratne

--- epstein_files/documents/emails/emailers.py
+++ epstein_files/documents/emails/emailers.py
@@ -50,9 +50,9 @@ ADDITIONAL_EMAILERS = [
     Entity('Peter Green', match_partial=None),
     Entity('Sarah Mapes'),
     # Krassner CCs
-    Entity('Caryl Ratner', match_partial=None, is_interesting=False),
     Entity('Barb Cowles', match_partial=None, is_interesting=False),
     Entity('Bob Fass', match_partial=None, is_interesting=False),
+    Entity('Caryl Ratner', match_partial=None, is_interesting=False),
     Entity('Daniel Dawson', match_partial=None, is_interesting=False),
     Entity('Holly Krassner Dawson', match_partial=None, is_interesting=False),
     Entity('Lee Quarnstrom', match_partial=None, is_interesting=False),
