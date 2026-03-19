4f6c7f14318825274e0845d74be5c21dbbeca913
ashariyar <ashariyar@users.noreply.github.com>
2026-03-18 20:46:28 -0400

    bash lib

--- scripts/bash_lib/shared.sh
+++ scripts/bash_lib/shared.sh
@@ -1,56 +1,76 @@
 # bash functions and variables shared by multiple scripts.
 source .env
-
-CURRENT_BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-GENERATE_CMD='epstein_generate --build --suppress-output'
-GENERATE_MOBILE_CMD="$GENERATE_CMD --mobile"
 LIB_DIR=$(dirname -- "$(readlink -f -- "$0";)";)
 SCRIPT_DIR=$(realpath "$LIB_DIR/..")
 
-# echo -e "shared.sh LIB_DIR=$LIB_DIR, SCRIPT_DIR=$SCRIPT_DIR"
+CURRENT_BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+GH_PAGES_BRANCH=gh_pages
+GENERATE_CMD='epstein_generate --build --suppress-output'
+GENERATE_MOBILE_CMD="$GENERATE_CMD --mobile"
+
 
 if [ -n "$BASH_COLORS_PATH" ]; then
     source "$BASH_COLORS_PATH"
     clr_cyan "Sourced '$(clr_green $BASH_COLORS_PATH)'..."
+    ERROR_PFX="$(clr_black clr_redb clr_bold ERROR):"
+    WARNING_PFX="$(clr_black clr_brownb clr_bold WARNING):"
 else
     echo -e "bash colors not found, can't print status msgs. BASH_COLORS_PATH=$BASH_COLORS_PATH"
     exit 1
 fi
 
-
-print_deploy_step() {
-    echo -e ""
-    clr_cyan "$1"
+print_error() {
+    echo -e "\n    $ERROR_PFX $(clr_cyan "$1") $(clr_magenta "$2")\n"
 }
 
-print_msg() {
-    local msg="$1"
-    local colored_part="$2"
-
-    if [ -n "$colored_part" ]; then
-        msg="$msg '$(clr_green $colored_part)'"
-    fi
-
-    clr_cyan "$msg"
+print_warning() {
+    echo -e "\n    $WARNING_PFX $(clr_cyan "$1") $(clr_magenta "$2")\n"
 }
+
 
 # Running 'bash -l' uses the login shell but then the poetry venv isn't set :(
 any_uncommitted_changes() {
     if [[ $(git status --porcelain --untracked-files=no | wc -l) -eq 0 ]]; then
         return 1
     else
+        print_error "Uncommitted changes on branch" $CURRENT_BRANCH
         return 0
     fi
 }
 
+
 commit_gh_pages_changes() {
     echo -e ""
-    print_deploy_step "Committing changes to gh_pages..."
+    print_deploy_step "Committing changes to" $GH_PAGES_BRANCH
     git commit -am"Update HTML"
-    git push origin gh_pages --quiet
+    git push origin $GH_PAGES_BRANCH --quiet
 }
+
 
 copy_custom_html() {
     print_deploy_step "Copying custom HTML pages into place..."
     epstein_generate --use-custom-html
+}
+
+
+# Ensure we're on the master branch with no uncommitted changes
+ensure_safe_branch() {
+    if [[ $CURRENT_BRANCH != "master" ]]; then
+        clr_red "ERROR: Current branch is not master: ($CURRENT_BRANCH)"
+        exit 1
+    elif any_uncommitted_changes; then
+        clr_red "ERROR: Uncommitted changes"
+        exit 1
+    fi
+}
+
+
+print_deploy_step() {
+    local msg="$(clr_cyan "$1")"
+
+    if [[ -n "$2" ]]; then
+        msg="$msg '$(clr_green $2)'"
+    fi
+
+    echo -e "$msg..."
 }
