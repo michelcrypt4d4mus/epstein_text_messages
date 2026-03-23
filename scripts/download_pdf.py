ed69a37929513be7047afd92830e79c37ed5d442
ashariyar <ashariyar@users.noreply.github.com>
2026-03-23 18:37:12 -0400

    is_int

--- scripts/download_pdf.py
+++ scripts/download_pdf.py
@@ -4,13 +4,13 @@ from epstein_files.people.names import *
 from epstein_files.util.constant.urls import download_jmail_pdf
 from epstein_files.util.env import args
 from epstein_files.util.helpers.file_helper import extract_file_id, open_file_or_url
-from epstein_files.util.helpers.string_helper import INTEGER_REGEX
+from epstein_files.util.helpers.string_helper import is_integer
 from epstein_files.util.logging import exit_with_error, logger
 
 
 if len(args.positional_args) != 2:
     exit_with_error(f"two positional args expected (ID and dataset number)")
-elif not INTEGER_REGEX.match(args.positional_args[0]):
+elif not is_integer(args.positional_args[0]):
     exit_with_error(f"2nd argument should be a number, instead it's {args.positional_args[0]}")
 
 file_id = extract_file_id(args.positional_args[1].upper())
