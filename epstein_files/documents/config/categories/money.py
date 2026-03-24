9b597efe2965ef9ce7e7a151c7bb6304de6306b3
ashariyar <ashariyar@users.noreply.github.com>
2026-03-24 10:02:35 -0400

    zwirn

--- epstein_files/documents/config/categories/money.py
+++ epstein_files/documents/config/categories/money.py
@@ -24,6 +24,7 @@ MONEY_OCR_REPAIRS: OcrRepair = {
     'Southern Trust Company,\nInc.\n$': 'Southern Trust Company, Inc $',
     'Black do\nApollo Management': 'Black c/o Apollo Management',
     re.compile(r'Boothbay Absolute Strategies\nFund LP\n\$'): "Boothbay Absolute Strategies Fund LP $",
+    'D.B. Zwim': 'D.B. Zwirn',
 }
 
 
