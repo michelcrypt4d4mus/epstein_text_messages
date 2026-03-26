ebcedda4aadb788374aa0afdc4df576a90b18ddd
ashariyar <ashariyar@users.noreply.github.com>
2026-03-26 16:16:04 -0400

    jmail_download_bad_ids() contextmanager

--- scripts/extract_doj_pdfs.py
+++ scripts/extract_doj_pdfs.py
@@ -3,10 +3,8 @@
 # Requires the EPSTEIN_DOJ_PDFS_20260130_DIR env var to be set.
 import re
 import shutil
-import sys
 
 from pdfalyzer.decorators.pdf_file import PdfFile
-from rich.prompt import Confirm, Prompt
 from yaralyzer.util.helpers.interaction_helper import ask_to_proceed
 from yaralyzer.util.helpers.shell_helper import ShellResult
 
@@ -15,15 +13,12 @@ from epstein_files.documents.document import DOJ_DATASET_ID_REGEX
 from epstein_files.output.rich import console
 from epstein_files.util.constant.strings import DOJ_FILE_NAME_REGEX
 from epstein_files.util.env import DOJ_PDFS_20260130_DIR, DOJ_TXTS_20260130_DIR, DOJ_PDFS_20260130_DIR_ENV_VAR, args
-from epstein_files.util.helpers.env_helpers import get_env_dir
-from epstein_files.util.helpers.file_helper import extract_file_id
+from epstein_files.util.helpers.file_helper import broken_pdfs_dir, extract_file_id, jmail_download_bad_ids
 from epstein_files.util.logging import logger
 
 assert DOJ_PDFS_20260130_DIR is not None, f"{DOJ_PDFS_20260130_DIR_ENV_VAR} env var is not set!"
 assert DOJ_TXTS_20260130_DIR is not None
 
-BROKEN_PDFS_DIR = get_env_dir('BROKEN_PDFS_DIR', must_exist=True)
-KNOWN_BAD_JMAIL_IDS_PATH = BROKEN_PDFS_DIR.joinpath('broken_jmail_ids.txt')
 JMAIL_FILENAME_REGEX = re.compile(r"vol(\d+)-(?:official-doj-latest-)?(efta\d+)(-pdf)?.pdf")
 BAD_FILENAME_REGEX = re.compile(r".*/EFTA\d+-\d\.pdf")
 EXTRACT_ARGS = ['extract_pdf_text', '--no-page-number-panels', '--panelize-image-text']
@@ -33,66 +28,60 @@ if not DOJ_TXTS_20260130_DIR.exists():
     ask_to_proceed(f"Dir {DOJ_TXTS_20260130_DIR} doesn't exist, create?")
     DOJ_TXTS_20260130_DIR.mkdir()
 
-if KNOWN_BAD_JMAIL_IDS_PATH.exists():
-    known_bad_ids = set(KNOWN_BAD_JMAIL_IDS_PATH.read_text().split('\n'))
-    logger.warning(f"Loaded {known_bad_ids} known bad IDs from '{KNOWN_BAD_JMAIL_IDS_PATH}'...")
-else:
-    known_bad_ids = set([])
 
 skipped = 0
 
-for dir in [d for d in DOJ_PDFS_20260130_DIR.glob('*') if d.is_dir()]:
-    if (dir_match := DOJ_DATASET_ID_REGEX.search(dir.name)):
-        dataset_number = int(dir_match.group(1))
-        logger.info(f"Processing DataSet {dataset_number} (dir: '{dir}')")
-    else:
-        logger.debug(f"Skipping dir: '{dir}'")
-        continue
-
-    extracted_text_dir = DOJ_TXTS_20260130_DIR.joinpath(dir.name)
-
-    if not extracted_text_dir.exists():
-        logger.warning(f"Export dir '{extracted_text_dir}' does not exist, creating...")
-        extracted_text_dir.mkdir()
-
-    for pdf_path in dir.glob('**/*.pdf'):
-        if (jmail_match := JMAIL_FILENAME_REGEX.match(pdf_path.name)):
-            new_pdf_path = pdf_path.parent.joinpath(jmail_match.group(2).upper() + '.pdf')
-            logger.warning(f"Found Jmail PDF '{pdf_path}'\n      moving to '{new_pdf_path}'")
-            shutil.move(pdf_path, new_pdf_path)
-            pdf_path = new_pdf_path
-        elif not DOJ_FILE_NAME_REGEX.match(pdf_path.name):
-            raise RuntimeError(f"Bad filename '{pdf_path}'!")
-
-        txt_file_path = extracted_text_dir.joinpath(pdf_path.stem + '.txt')
-
-        if txt_file_path.exists():
-            logger.info(f"Skipping file that already exists '{pdf_path}' in .txt format...")
-
-            if skipped > 0 and skipped % 500 == 0:
-                logger.warning(f"Skipped {skipped} PDFs that already exist as .txt...")
-
-            skipped += 1
+with jmail_download_bad_ids() as known_bad_ids:
+    for dir in [d for d in DOJ_PDFS_20260130_DIR.glob('*') if d.is_dir()]:
+        if (dir_match := DOJ_DATASET_ID_REGEX.search(dir.name)):
+            dataset_number = int(dir_match.group(1))
+            logger.info(f"Processing DataSet {dataset_number} (dir: '{dir}')")
+        else:
+            logger.debug(f"Skipping dir: '{dir}'")
             continue
 
-        pdf_file = PdfFile(pdf_path)
-        cmd = EXTRACT_ARGS + [str(pdf_path), '--output-dir', str(extracted_text_dir)]
-        logger.debug(f"Processing file ({pdf_file.file_size / 1024:.1f} kb): '{pdf_path}': ")
-        console.print(f"        " + ' '.join(cmd), style='wheat4')
-        result = ShellResult.from_cmd(cmd, verify_success=True)
-        logger.debug(result.output_logs(), extra={"highlighter": None})
-        console.line()
+        extracted_text_dir = DOJ_TXTS_20260130_DIR.joinpath(dir.name)
 
-        if not txt_file_path.exists():
-            if pdf_path.read_text().startswith('<!DOCTYPE html>'):
-                logger.error(f"'{pdf_path}' is an HTML file, not a PDF, moving to '{BROKEN_PDFS_DIR}'...")
-                known_bad_ids.add(extract_file_id(pdf_path))
-            else:
-                logger.error(f"Failed to extract any text from {pdf_path}, moving to '{BROKEN_PDFS_DIR}'...")
+        if not extracted_text_dir.exists():
+            logger.warning(f"Export dir '{extracted_text_dir}' does not exist, creating...")
+            extracted_text_dir.mkdir()
 
-            shutil.move(pdf_path, BROKEN_PDFS_DIR.joinpath(pdf_path.name))
+        for pdf_path in dir.glob('**/*.pdf'):
+            if (jmail_match := JMAIL_FILENAME_REGEX.match(pdf_path.name)):
+                new_pdf_path = pdf_path.parent.joinpath(jmail_match.group(2).upper() + '.pdf')
+                logger.warning(f"Found Jmail PDF '{pdf_path}'\n      moving to '{new_pdf_path}'")
+                shutil.move(pdf_path, new_pdf_path)
+                pdf_path = new_pdf_path
+            elif not DOJ_FILE_NAME_REGEX.match(pdf_path.name):
+                raise RuntimeError(f"Bad filename '{pdf_path}'!")
 
-KNOWN_BAD_JMAIL_IDS_PATH.write_text('\n'.join(known_bad_ids))
-logger.warning(f"Wrote {len(known_bad_ids)} known bad IDs to '{KNOWN_BAD_JMAIL_IDS_PATH}'")
+            txt_file_path = extracted_text_dir.joinpath(pdf_path.stem + '.txt')
+
+            if txt_file_path.exists():
+                logger.info(f"Skipping file that already exists '{pdf_path}' in .txt format...")
+
+                if skipped > 0 and skipped % 500 == 0:
+                    logger.warning(f"Skipped {skipped} PDFs that already exist as .txt...")
+
+                skipped += 1
+                continue
+
+            pdf_file = PdfFile(pdf_path)
+            cmd = EXTRACT_ARGS + [str(pdf_path), '--output-dir', str(extracted_text_dir)]
+            logger.debug(f"Processing file ({pdf_file.file_size / 1024:.1f} kb): '{pdf_path}': ")
+            console.print(f"        " + ' '.join(cmd), style='wheat4')
+            result = ShellResult.from_cmd(cmd, verify_success=True)
+            logger.debug(result.output_logs(), extra={"highlighter": None})
+            console.line()
+
+            if not txt_file_path.exists():
+                if pdf_path.read_text().startswith('<!DOCTYPE html>'):
+                    logger.error(f"'{pdf_path}' is an HTML file, not a PDF, moving to '{broken_pdfs_dir()}'...")
+                    known_bad_ids.add(extract_file_id(pdf_path))
+                else:
+                    logger.error(f"Failed to extract any text from {pdf_path}, moving to '{broken_pdfs_dir()}'...")
+
+                shutil.move(pdf_path, broken_pdfs_dir().joinpath(pdf_path.name))
+
 args.constantize = True
 EpsteinFiles.get_files().load_new_files()
