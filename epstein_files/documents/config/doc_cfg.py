c72aba409cefee6c1a9024134217a1538e3b2e2e
ashariyar <ashariyar@users.noreply.github.com>
2026-04-01 23:41:40 -0400

    cfg

--- epstein_files/documents/config/doc_cfg.py
+++ epstein_files/documents/config/doc_cfg.py
@@ -29,12 +29,14 @@ Metadata = dict[str, bool | datetime | int | str | None | list[str | None] | dic
 DuplicateType = Literal['bounced', 'bad_ocr', 'earlier', 'quoted', 'redacted', 'same', 'screenshot', 'version']
 
 AUTO_QUOTE_NUM_CHARS = 250 if args.output_most_interesting else 400  # Number of chars before and after highlight_quote for auto truncation
-DOC_CHAR_RANGE = (0, 4_000)
-EMAIL_TRUNCATE_TO = int(DOC_CHAR_RANGE[1] / 3)
 MAX_REPR_LINE_LENGTH = 135
-NO_TRUNCATE = -1
-SHORT_TRUNCATE_TO = int(EMAIL_TRUNCATE_TO / 3)
 WHOLE_FILE_CHAR_RANGE = (0, 10_000_000_000)
+
+# Char range / truncation constants
+DOC_CHAR_RANGE = (0, 4_000)
+EMAIL_TRUNCATE_TO = int(DOC_CHAR_RANGE[1] / 2)
+SHORT_TRUNCATE_TO = int(EMAIL_TRUNCATE_TO / 3)
+NO_TRUNCATE = -1
 
 CHECK_LINK_FOR_DETAILS = 'not shown here, check original PDF for details'
 QUOTE_PREFIX = 'see quote'
