adb54310d821dd33deb74ce6405a0644d95b45e4
ashariyar <ashariyar@users.noreply.github.com>
2026-09-20 17:38:29 -0400

    configs

--- epstein_files/documents/config/categories/girls.py
+++ epstein_files/documents/config/categories/girls.py
@@ -1320,6 +1320,7 @@ GIRLS_CFGS = [
     EmailCfg(id='EFTA00458715', show_with_name=MASHA_DROKOVA),
     EmailCfg(id='EFTA01036594', show_with_name=MASHA_DROKOVA, truncate_to=NO_TRUNCATE, recipients=[LESLEY_GROFF], recipient_uncertain=True),
     EmailCfg(id='EFTA01009995', show_with_name=MASHA_DROKOVA, truncate_to=NO_TRUNCATE),
+    EmailCfg(id='EFTA00880881', highlight_quote="she is trustworthy", is_interesting=5),
     EmailCfg(id='EFTA00349192', note='contains visa info'),
     blaine_letter('019086', date='2015-05-27', suffix='naming various Putin puppet regimes', show_full_panel=True),
     blaine_letter('019474', date='2015-05-29'),
