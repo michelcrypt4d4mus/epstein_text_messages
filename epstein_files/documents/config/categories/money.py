cbea87445cf12cab074000a6d291ecd36330e89e
ashariyar <ashariyar@users.noreply.github.com>
2026-09-20 20:18:26 -0400

    cfg

--- epstein_files/documents/config/categories/money.py
+++ epstein_files/documents/config/categories/money.py
@@ -389,6 +389,9 @@ MONEY_CFGS = [
         truncate_to=(552, 1_300),
     ),
 
+    # Jon Tomlinson / Anastasiya Siro Siroochenko
+    EmailCfg(id='EFTA02347233', highlight_quote='I maintain 3 sets of books', is_interesting=5),
+
     # Junkermann
     EmailCfg(id='EFTA01810549', recipients=[NICOLE_JUNKERMANN], author_reason='visible in replies', truncate_to=605),
     EmailCfg(id='EFTA02435074', note=f"Epstein introduces {LEON_BLACK} and {NICOLE_JUNKERMANN}", truncate_to=(1_600, 9_000)),
